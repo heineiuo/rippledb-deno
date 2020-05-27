ff38fb5818bd103defdb9937a9cf9cd0f445ba21
heineiuo <heineiuo@gmail.com>
2020-05-27 01:34:12 +0000

    Update Rippledb Deno

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export { default as InternalDatabase } from "./Database";
-export { WriteBatch } from "./WriteBatch";
-export { Options, ReadOptions, DatabaseOptions, WriteOptions, IteratorOptions, } from "./Options";
-export { Env } from "./Env";
+export { default as InternalDatabase } from "./Database.ts";
+export { WriteBatch } from "./WriteBatch.ts";
+export { Options, ReadOptions, DatabaseOptions, WriteOptions, IteratorOptions, } from "./Options.ts";
+export { Env } from "./Env.ts";
